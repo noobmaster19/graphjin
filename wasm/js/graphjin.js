e058a8ae7552f4822e4795dce2047534caff62f3
NoobMaster19 <48677212+noobmaster19@users.noreply.github.com>
2024-09-08 17:28:47 +0800

    Update graphjin.js

--- wasm/js/graphjin.js
+++ wasm/js/graphjin.js
@@ -11,7 +11,7 @@ const go = new Go();
 
 // Modify the function to accept an fs-like object as a parameter
 export default async function(configPath, config, db, customFs = undefined) {
-    const fs = customFs || (await import('fs'));  // Use the provided fs-like object, or fall back to Node's fs
+    const fs = (await import('fs'));  // Use the provided fs-like object, or fall back to Node's fs
 
     const wasmFile = fs.readFileSync(join(__dirname, "../graphjin.wasm"));
     const inst = await WebAssembly.instantiate(wasmFile, go.importObject);
@@ -20,9 +20,9 @@ export default async function(configPath, config, db, customFs = undefined) {
     // Handle configuration and pass the custom fs object
     if (typeof config === 'string') {
         const conf = { value: config, isFile: true };
-        return await createGraphJin(configPath, conf, db, fs);
+        return await createGraphJin(configPath, conf, db, customFs);
     } else {
         const conf = { value: JSON.stringify(config), isFile: false };
-        return await createGraphJin(configPath, conf, db, fs);
+        return await createGraphJin(configPath, conf, db, customFs);
     }
 }
