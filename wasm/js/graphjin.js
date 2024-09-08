44bd8b2a38f66161c3206318bfbaf718cab40383
NoobMaster19 <48677212+noobmaster19@users.noreply.github.com>
2024-09-08 17:20:10 +0800

    Allow custom VFS
    Allows the user to dynamically pass configurations

--- wasm/js/graphjin.js
+++ wasm/js/graphjin.js
@@ -1,7 +1,5 @@
-import "./globals.js"
+import "./globals.js";
 import * as _ from "./wasm_exec.js";
-
-import fs from "fs"
 
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
@@ -10,18 +8,21 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const go = new Go();
-const f = fs.readFileSync(join(__dirname,"../graphjin.wasm"));
-const inst = await WebAssembly.instantiate(f, go.importObject);
-go.run(inst.instance);
 
-// TODO: Use NODE_ENV to set production mode
+// Modify the function to accept an fs-like object as a parameter
+export default async function(configPath, config, db, customFs = undefined) {
+    const fs = customFs || (await import('fs'));  // Use the provided fs-like object, or fall back to Node's fs
 
-export default async function(configPath, config, db) {
+    const wasmFile = fs.readFileSync(join(__dirname, "../graphjin.wasm"));
+    const inst = await WebAssembly.instantiate(wasmFile, go.importObject);
+    go.run(inst.instance);
+
+    // Handle configuration and pass the custom fs object
     if (typeof config === 'string') {
-        const conf = {value: config, isFile: true}
-        return await createGraphJin(configPath, conf, db, fs)
+        const conf = { value: config, isFile: true };
+        return await createGraphJin(configPath, conf, db, fs);
     } else {
-        const conf = {value: JSON.stringify(config), isFile: false}
-        return await createGraphJin(configPath, conf, db, fs) 
+        const conf = { value: JSON.stringify(config), isFile: false };
+        return await createGraphJin(configPath, conf, db, fs);
     }
 }
